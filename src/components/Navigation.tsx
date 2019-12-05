d467b5d7ae85c28d0d46d502874041b477ffa659
fragosti <francesco.agosti93@gmail.com>
2019-12-05 13:27:04 -0800

    Add and use mesh logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,12 +3,12 @@ import { Flex, Text } from 'rebass';
 
 import { colors } from '../theme';
 
-import { Logo } from './Logo';
+import { MeshLogo } from './MeshLogo';
 
 export const Navigation: React.FC = () => (
   <Flex px={3} py={3} color="0x Mesh Network" bg={colors.black} alignItems="center">
-    <Logo />
-    <Text p={2} color={colors.whiteText} fontSize={32}>
+    <MeshLogo />
+    <Text ml={2} p={2} color={colors.whiteText} fontSize={32}>
       0x Mesh Network
     </Text>
   </Flex>
--- src/components/ZeroExLogo.tsx
+++ src/components/ZeroExLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Logo: React.FC = () => (
+export const ZeroExLogo: React.FC = () => (
   <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect width="50" height="50" rx="25" fill="black" />
     <path
