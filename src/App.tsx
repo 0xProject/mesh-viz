492c2874dbd7fa263a0e7fece2f6723f3c76dfee
fragosti <francesco.agosti93@gmail.com>
2019-12-05 11:35:58 -0800

    Notices are not supported in react so removed them

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,6 @@ const traffic = {
       name: 'mesh-node-1',
       // OPTIONAL Override the name on the label
       displayName: 'mesh-node-1',
-      // OPTIONAL Any notices that you want to show up in the sidebar, for more details check the section on notices.
-      notices: [
-        {
-          // The title to display on the notice
-          title: 'Notice about something',
-          // OPTIONAL link to send the user when click on the notice
-          link: 'http://link/to/relevant/thing',
-          // OPTIONAL 0(default) for info level, 1 for warning level, 2 for error level (applies CSS styling)
-          severity: 1,
-        },
-      ],
       //  The class of the node. will default to 'normal' if not provided. The coloring of the UI is based on 'normal', 'warning', and 'danger', so if you want to match the UI coloring, use those class names. Any class you provide will expect to have a style 'colorClassName' available, e.g. if the class is 'fuzzy', you should also call 'vizceral.updateStyles({ colorTraffic: { fuzzy: '#aaaaaa' } })'
       class: 'normal',
       // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
@@ -39,17 +28,6 @@ const traffic = {
       name: 'mesh-node-2',
       // OPTIONAL Override the name on the label
       displayName: 'mesh-node-2',
-      // OPTIONAL Any notices that you want to show up in the sidebar, for more details check the section on notices.
-      notices: [
-        {
-          // The title to display on the notice
-          title: 'Notice about something',
-          // OPTIONAL link to send the user when click on the notice
-          link: 'http://link/to/relevant/thing',
-          // OPTIONAL 0(default) for info level, 1 for warning level, 2 for error level (applies CSS styling)
-          severity: 1,
-        },
-      ],
       //  The class of the node. will default to 'normal' if not provided. The coloring of the UI is based on 'normal', 'warning', and 'danger', so if you want to match the UI coloring, use those class names. Any class you provide will expect to have a style 'colorClassName' available, e.g. if the class is 'fuzzy', you should also call 'vizceral.updateStyles({ colorTraffic: { fuzzy: '#aaaaaa' } })'
       class: 'normal',
       // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
