11a2961e3bcbfc19cc3739ba8e2114111990ccd8
fragosti <francesco.agosti93@gmail.com>
2019-12-05 13:51:11 -0800

    Add more mesh nodes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -38,6 +38,24 @@ const traffic = {
       // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
       metadata: {},
     },
+    {
+      name: 'mesh-node-3',
+      // OPTIONAL Override the name on the label
+      displayName: 'mesh-node-3',
+      //  The class of the node. will default to 'normal' if not provided. The coloring of the UI is based on 'normal', 'warning', and 'danger', so if you want to match the UI coloring, use those class names. Any class you provide will expect to have a style 'colorClassName' available, e.g. if the class is 'fuzzy', you should also call 'vizceral.updateStyles({ colorTraffic: { fuzzy: '#aaaaaa' } })'
+      class: 'normal',
+      // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
+      metadata: {},
+    },
+    {
+      name: 'mesh-node-4',
+      // OPTIONAL Override the name on the label
+      displayName: 'mesh-node-4',
+      //  The class of the node. will default to 'normal' if not provided. The coloring of the UI is based on 'normal', 'warning', and 'danger', so if you want to match the UI coloring, use those class names. Any class you provide will expect to have a style 'colorClassName' available, e.g. if the class is 'fuzzy', you should also call 'vizceral.updateStyles({ colorTraffic: { fuzzy: '#aaaaaa' } })'
+      class: 'normal',
+      // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
+      metadata: {},
+    },
   ],
   connections: [
     {
@@ -45,6 +63,38 @@ const traffic = {
       source: 'mesh-node-1',
       // The target node of the connection, will log a warning if the node does not exist.
       target: 'mesh-node-2',
+      // These are the three default types/colors available in the component and the colors that are used for the nodes themselves. You are welcme to add others, or use other names instead knowing tha they may not match the UI coloring appropriately.
+      metrics: {
+        normal: 5000,
+        danger: 5,
+        warning: 0,
+      },
+      // OPTIONAL Any notices that you want to show up in the sidebar
+      notices: [],
+      // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
+      metadata: {},
+    },
+    {
+      // The source node of the connection, will log a warning if the node does not exist.
+      source: 'mesh-node-3',
+      // The target node of the connection, will log a warning if the node does not exist.
+      target: 'mesh-node-4',
+      // These are the three default types/colors available in the component and the colors that are used for the nodes themselves. You are welcme to add others, or use other names instead knowing tha they may not match the UI coloring appropriately.
+      metrics: {
+        normal: 5000,
+        danger: 5,
+        warning: 0,
+      },
+      // OPTIONAL Any notices that you want to show up in the sidebar
+      notices: [],
+      // OPTIONAL Any data that may be handled by a plugin or other data that isn't important to vizceral itself (if you want to show stuff on the page that contains vizceral, for example). Since it is completely optional and not handled by vizceral, you technically could use any index, but this is the convention we use.
+      metadata: {},
+    },
+    {
+      // The source node of the connection, will log a warning if the node does not exist.
+      source: 'mesh-node-2',
+      // The target node of the connection, will log a warning if the node does not exist.
+      target: 'mesh-node-4',
       // These are the three default types/colors available in the component and the colors that are used for the nodes themselves. You are welcme to add others, or use other names instead knowing tha they may not match the UI coloring appropriately.
       metrics: {
         normal: 5000,
